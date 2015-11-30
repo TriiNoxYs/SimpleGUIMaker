9f5abca3b5143861be6de774d77b2dc950ea335f
TriiNoxYs <triinoxys@gmail.com>
2015-11-30 04:07:35 +0100

    Fix /sgm list

--- src/fr/TriiNoxYs/SimpleGUIMaker/handlers/ListInvs.java
+++ src/fr/TriiNoxYs/SimpleGUIMaker/handlers/ListInvs.java
@@ -9,17 +9,12 @@ import fr.TriiNoxYs.SimpleGUIMaker.Main;
 public class ListInvs{
 
     public ListInvs(Main main, Player player) {
-        if (main.inventories.data.getString(player.getUniqueId().toString()) == null)
-            player.sendMessage("§7You don't have any saved inventories.");
-        else {
-            //Set<String> inventories = main.inventories.data.getConfigurationSection(player.getUniqueId().toString()).getKeys(true);
-            Set<String> inventories = main.inventories.data.getKeys(false);
-            player.sendMessage("§7Saved inventories:");
-            ArrayList<String> invs = new ArrayList<String>();
-            for (String inv : inventories)
-                if (!inv.contains("."))
-                    invs.add(inv);
-            player.sendMessage("§a" + invs.toString());
-        }
+    	Set<String> inventories = main.inventories.data.getKeys(false);
+    	player.sendMessage("§7Saved inventories:");
+    	ArrayList<String> invs = new ArrayList<String>();
+    	for (String inv : inventories)
+    		if (!inv.contains("."))
+    			invs.add(inv);
+    	player.sendMessage("§a" + invs.toString());
     }
 }
